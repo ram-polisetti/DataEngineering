62c4bf048b82e0a17f50171267b75a4e29929c6e
ramCharan polisetti <saitejapolisetti284@gmail.com>
2024-01-17 06:51:38 +0000

    created docker file

--- Week1/DockerizingIngestionScript/upload_data.py
+++ Week1/DockerizingIngestionScript/upload_data.py
@@ -1,7 +1,7 @@
 
 import pandas as pd
 from sqlalchemy import create_engine
-!pip install sqlalchemy --quiet
+# !pip install sqlalchemy --quiet
 import argparse
 
 
@@ -19,12 +19,20 @@ def main(params):
     database = params.database
     table = params.table
     url = params.url
-    taxi_data = pd.read_csv(url)
-    engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{database}')
-    taxi_data.to_sql(name=table, con=engine, if_exists='replace')
     
-
-
+    # engine = create_engine('postgresql://root:root@localhost:5432/ny_taxi')
+    engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{database}')
+    engine.connect()
+    
+    taxi_data = pd.read_csv(url, iterator=True, chunksize=100000)
+    df = next(taxi_data)
+    
+    df["tpep_pickup_datetime"] = pd.to_datetime(df["tpep_pickup_datetime"])
+    df["tpep_dropoff_datetime"] = pd.to_datetime(df["tpep_dropoff_datetime"])
+    
+    df.head(0).to_sql(name=table, con=engine, if_exists='replace')
+    df.to_sql(name=table, con=engine, if_exists='append')
+    
     try:
         while True:
             df = next(taxi_data)
@@ -43,29 +51,12 @@ parser = argparse.ArgumentParser(description="Ingest CSV data to Postgres")
 # table name
 # url of csv
 parser.add_argument('user', type=str, default='root', help='user name')
-parser.add_argument('password', type=str, default='<PASSWORD>', help='password')
+parser.add_argument('password', type=str, default='root', help='password')
 parser.add_argument('host', type=str, default='localhost', help='host')
 parser.add_argument('port', type=str, default='5432', help='port')
 parser.add_argument('database', type=str, default='ny_taxi', help='database name')
 parser.add_argument('table', type=str, default='yellow_taxi_data', help='name of the table where we will write the results')
 parser.add_argument('url', type=str, default='Yellow Taxi 2019 Jan.csv', help='url of csv')
-df = pd.read_csv('Yellow Taxi 2019 Jan.csv', nrows=100)
-
-df["tpep_pickup_datetime"] = pd.to_datetime(df["tpep_pickup_datetime"])
-
-df["tpep_dropoff_datetime"] = pd.to_datetime(df["tpep_dropoff_datetime"])
-
-
-engine = create_engine('postgresql://root:root@localhost:5432/ny_taxi')
-engine.connect()
-
-taxi_data = pd.read_csv('Yellow Taxi 2019 Jan.csv', iterator=True, chunksize=100000)
-
-tmp_df = next(taxi_data)
-
-tmp_df.head(0).to_sql(name='yellow_taxi_data', con=engine, if_exists='replace')
-
-tmp_df.to_sql(name='yellow_taxi_data', con=engine, if_exists='append')
 
 
 if __name__ == "__main__":
