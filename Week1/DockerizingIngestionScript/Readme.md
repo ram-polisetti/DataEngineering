420dffebf212ccf1921aa3de0bbd130a5ba5876e
ramCharan polisetti <saitejapolisetti284@gmail.com>
2024-01-17 07:14:10 +0000

    added Dockerfile

---
+++ Week1/DockerizingIngestionScript/Readme.md
@@ -0,0 +1,98 @@
+
+- Connecting to Postgres DB from jupyter
+
+
+```python
+from sqlalchemy import create_engine
+engine = create_engine('postgresql://root:root@localhost:5432/ny_taxi')
+engine.connect()
+```
+
+- postgresql -> db
+- root -> username
+- root -> password
+- 5432 -> portnumber
+- ny_taxi -> DB name
+
+![Alt text](image-1.png)
+
+![Alt text](image-2.png)
+
+```%time tmp_df.to_sql(name='yellow_taxi_data', con=engine, if_exists='append')```
+
+![Alt text](image-3.png)
+
+## Setting up Pgadmin Using Docker
+
+```docker
+docker pull dpage/pgadmin4
+```
+
+```docker   
+
+docker run -it \
+    -e PGADMIN_DEFAULT_EMAIL="admin@admin.com" \
+    -e PGADMIN_DEFAULT_PASSWORD="root" \
+    -p 8080:80 \
+    dpage/pgadmin4
+```
+
+- Doing this eventually fails cause postgredb and pgadmin are in different networks and they cant communicate with each other
+
+## Creating Network 
+
+```docker
+docker network create pg-network
+```
+
+- postgresdb
+```docker
+docker run -it \
+    -e POSTGRES_USER="root" \
+    -e POSTGRES_PASSWORD="root" \
+    -e POSTGRES_DB="ny_taxi" \
+    -v $(pwd)/ny_taxi_postgres_data:/var/lib/postgresql/data \
+    -p 5432:5432 \
+    --network=pg-network \
+    --name=pg-database \ 
+    postgres:13
+```
+
+- --network -> assign the network name to this variable
+- --name -> how the pgadmin is going to discover postgres db
+
+
+- Now we have to run pg-admin in the same network 
+
+```docker   
+
+docker run -it \
+    -e PGADMIN_DEFAULT_EMAIL="admin@admin.com" \
+    -e PGADMIN_DEFAULT_PASSWORD="root" \
+    -p 8080:80 \
+    --network=pg-network \
+    --name=pgadmin \ 
+    dpage/pgadmin4
+```
+
+- here the name is not important because we are not connecting any container to pgadmin whereas we need connection to postgresdb to access the data.
+- Login using credentials "admin@admin.com" and "root"
+  - Right click on servers 
+  - create new server
+  - hostname -> pg-database
+  - username -> root
+  - password -> root
+- Then go to databases and inspect the ny_taxi database
+![Alt text](image-4.png)
+
+
+## Python ingestion for dockerfile
+
+```docker
+python upload_data.py \
+--user=root \
+--password=root \
+--host=localhost \
+--port=5432 \
+--dbname=ny_taxi
+```
--- Week1/DockerizingIngestionScript/upload_data.py
+++ Week1/DockerizingIngestionScript/upload_data.py
@@ -3,6 +3,7 @@ import pandas as pd
 from sqlalchemy import create_engine
 # !pip install sqlalchemy --quiet
 import argparse
+import os
 
 
 
@@ -20,11 +21,14 @@ def main(params):
     table = params.table
     url = params.url
     
+    csv_name = 'output.csv'
+    os.system(f'wget {url} -O {csv_name}')
+    
     # engine = create_engine('postgresql://root:root@localhost:5432/ny_taxi')
     engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{database}')
     engine.connect()
     
-    taxi_data = pd.read_csv(url, iterator=True, chunksize=100000)
+    taxi_data = pd.read_csv(csv_name, iterator=True, chunksize=100000)
     df = next(taxi_data)
     
     df["tpep_pickup_datetime"] = pd.to_datetime(df["tpep_pickup_datetime"])
@@ -42,22 +46,28 @@ def main(params):
     except StopIteration:
         print('Finished Ingesting data')
     
-parser = argparse.ArgumentParser(description="Ingest CSV data to Postgres")
-# user
-# password
-# host
-# port
-# database name
-# table name
-# url of csv
-parser.add_argument('user', type=str, default='root', help='user name')
-parser.add_argument('password', type=str, default='root', help='password')
-parser.add_argument('host', type=str, default='localhost', help='host')
-parser.add_argument('port', type=str, default='5432', help='port')
-parser.add_argument('database', type=str, default='ny_taxi', help='database name')
-parser.add_argument('table', type=str, default='yellow_taxi_data', help='name of the table where we will write the results')
-parser.add_argument('url', type=str, default='Yellow Taxi 2019 Jan.csv', help='url of csv')
+
+
 
 
 if __name__ == "__main__":
-        main(parser.parse_args())
+    
+    parser = argparse.ArgumentParser(description="Ingest CSV data to Postgres")
+    # user
+    # password
+    # host
+    # port
+    # database name
+    # table name
+    # url of csv
+    parser.add_argument('user', type=str, default='root', help='user name')
+    parser.add_argument('password', type=str, default='root', help='password')
+    parser.add_argument('host', type=str, default='localhost', help='host')
+    parser.add_argument('port', type=str, default='5432', help='port')
+    parser.add_argument('database', type=str, default='ny_taxi', help='database name')
+    parser.add_argument('table', type=str, default='yellow_taxi_data', help='name of the table where we will write the results')
+    parser.add_argument('url', type=str, default='Yellow Taxi 2019 Jan.csv', help='url of csv')
+    
+    args = parser.parse_args()
+    # print(args.accumulate(args.integers))
+    main(args)
