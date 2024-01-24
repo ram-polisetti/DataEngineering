ce5e473a3052bc60a1a02fcabf9a920721da2d84
ram-polisetti <saitejapolisetti284@gmail.com>
2024-01-24 01:26:21 +0000

    completed 7.3

--- 7.ApacheSpark/7.3/vendor_monthly_revenue.sql
+++ 7.ApacheSpark/7.3/vendor_monthly_revenue.sql
@@ -1,7 +1,7 @@
 SELECT
     -- Reveneue grouping
     DATE_TRUNC('month', pickup_datetime) AS revenue_month,
-    pickup_zone AS revenue_zone,
+    VendorID as vendor_id,
     --Note: For BQ use instead: DATE_TRUNC(pickup_datetime, month) AS revenue_month,
     source,
 
@@ -10,16 +10,14 @@ SELECT
     SUM(tip_amount) AS revenue_monthly_tip_amount,
     SUM(tolls_amount) AS revenue_monthly_tolls_amount,
     SUM(extra) AS revenue_monthly_extra,
-    SUM(ehail_fee) AS revenue_monthly_ehail_fee,
     SUM(improvement_surcharge) AS revenue_monthly_improvement_surcharge,
     SUM(total_amount) AS revenue_monthly_total_amount,
     SUM(fare_amount) AS revenue_monthly_fare,
     SUM(congestion_surcharge) AS revenue_monthly_congestion_surcharge,
 
     -- Additional calculations
-    COUNT(tripid) AS total_monthly_trips,
     AVG(passenger_count) AS avg_montly_passenger_count,
     AVG(trip_distance) AS avg_montly_trip_distance
 
-    FROM trips_data
+    FROM trips
     GROUP BY 1,2,3
